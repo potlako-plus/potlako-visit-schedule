400e1ccb911047837663191179af78ea1f026402
ckgathi <ckgathi@gmail.com>
2020-01-08 15:56:19 +0200

    initial visit schedule

---
+++ potlako_visit_schedule/visit_schedules/visit_schedule.py
@@ -0,0 +1,15 @@
+from edc_visit_schedule import VisitSchedule, site_visit_schedules
+
+from .schedule import schedule1
+
+visit_schedule1 = VisitSchedule(
+    name='visit_schedule1',
+    verbose_name='potlako',
+    offstudy_model='potlako_prn.subjectoffstudy',
+    locator_model='potlako_subject.subject_locator',
+    death_report_model='potlako_subject.death_reportt',
+    previous_visit_schedule=None)
+
+visit_schedule1.add_schedule(schedule1)
+
+site_visit_schedules.register(visit_schedule1)
