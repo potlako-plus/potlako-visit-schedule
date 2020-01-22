f2fc44516ed0a5bedf048edb3a337f0c242209b7
ckgathi <ckgathi@gmail.com>
2020-01-22 16:12:19 +0200

    fix tying error

--- potlako_visit_schedule/visit_schedules/visit_schedule.py
+++ potlako_visit_schedule/visit_schedules/visit_schedule.py
@@ -7,7 +7,7 @@ visit_schedule1 = VisitSchedule(
     verbose_name='potlako',
     offstudy_model='potlako_prn.subjectoffstudy',
     locator_model='potlako_subject.subject_locator',
-    death_report_model='potlako_subject.death_reportt',
+    death_report_model='potlako_prn.death_report',
     previous_visit_schedule=None)
 
 visit_schedule1.add_schedule(schedule1)
