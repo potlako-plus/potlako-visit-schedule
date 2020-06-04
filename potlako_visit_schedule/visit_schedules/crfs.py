9b65d818bd9c8c94415a36024553b374e21fe493
tumiemosweu <imosweuk@gmail.com>
2020-06-04 16:13:46 +0200

    update visit schedule

--- potlako_visit_schedule/visit_schedules/crfs.py
+++ potlako_visit_schedule/visit_schedules/crfs.py
@@ -3,25 +3,31 @@ from edc_visit_schedule import FormsCollection, Crf
 crf = {}
 
 potlako_crfs_prn = FormsCollection(
-    Crf(show_order=1, model='potlako_subject.cliniciancallfollowup',
+    Crf(show_order=1, model='potlako_subject.medicaldiagnosis',
         required=False, additional=False),
-    Crf(show_order=2, model='potlako_subject.medicaldiagnosis',
+    Crf(show_order=2, model='potlako_subject.missedvisit',
+        required=False, additional=False),
+    Crf(show_order=3, model='potlako_subject.missedcall',
+        required=False, additional=False),
+    Crf(show_order=4, model='potlako_subject.sms',
         required=False, additional=False),
     name='potlako_crf_prn')
 
 crfs_1000 = FormsCollection(
     Crf(show_order=1, model='potlako_subject.patientcallinitial'),
-    Crf(show_order=4, model='potlako_subject.transport', required=False),
-    Crf(show_order=5, model='potlako_subject.homevisit', required=False),
-    Crf(show_order=6, model='potlako_subject.physicianreview', required=False),
+    Crf(show_order=2, model='potlako_subject.cliniciancallfollowup'),
+    Crf(show_order=3, model='potlako_subject.transport', required=False),
+    Crf(show_order=4, model='potlako_subject.homevisit', required=False),
+    Crf(show_order=5, model='potlako_subject.physicianreview', required=False),
     name='initial',
 )
 
 crfs_unscheduled = FormsCollection(
     Crf(show_order=1, model='potlako_subject.patientcallfollowup',),
-    Crf(show_order=4, model='potlako_subject.transport', required=False),
-    Crf(show_order=5, model='potlako_subject.homevisit', required=False),
-    Crf(show_order=6, model='potlako_subject.physicianreview', required=False),
+    Crf(show_order=2, model='potlako_subject.cliniciancallfollowup'),
+    Crf(show_order=3, model='potlako_subject.transport', required=False),
+    Crf(show_order=4, model='potlako_subject.homevisit', required=False),
+    Crf(show_order=5, model='potlako_subject.physicianreview', required=False),
     name='unscheduled',
 )
 
