18ed8db0cf088dfe3c5572c4ec08a0fb27d118df
tumiemosweu <imosweuk@gmail.com>
2020-09-08 15:45:49 +0200

    update enhanced care community form

--- potlako_visit_schedule/visit_schedules/crfs.py
+++ potlako_visit_schedule/visit_schedules/crfs.py
@@ -25,17 +25,19 @@ crfs_initial = FormsCollection(
 
 crfs_unscheduled = FormsCollection(
     Crf(show_order=1, model='potlako_subject.patientcallfollowup',),
-    Crf(show_order=2, model='potlako_subject.transport', required=False),
-    Crf(show_order=3, model='potlako_subject.homevisit', required=False),
-    Crf(show_order=4, model='potlako_subject.physicianreview', required=False),
-    Crf(show_order=5, model='potlako_subject.investigationsordered', required=False),
-    Crf(show_order=6, model='potlako_subject.investigationsresulted', required=False),
-    Crf(show_order=7, model='potlako_subject.medicaldiagnosis', required=False),
+    Crf(show_order=2, model='potlako_subject.cancerdiagnosisandtreatmentassessment',),
+    Crf(show_order=3, model='potlako_subject.transport', required=False),
+    Crf(show_order=4, model='potlako_subject.homevisit', required=False),
+    Crf(show_order=5, model='potlako_subject.physicianreview', required=False),
+    Crf(show_order=6, model='potlako_subject.investigationsordered', required=False),
+    Crf(show_order=7, model='potlako_subject.investigationsresulted', required=False),
+    Crf(show_order=8, model='potlako_subject.medicaldiagnosis', required=False),
     name='post_unscheduled',
 )
 
 crfs_followup = FormsCollection(
-    Crf(show_order=1, model='potlako_subject.cancerdiagnosisandtreatmentassessment'),
+    Crf(show_order=1, model='potlako_subject.patientcallfollowup'),
+    Crf(show_order=2, model='potlako_subject.cancerdiagnosisandtreatmentassessment'),
     name='followup',
 )
 
