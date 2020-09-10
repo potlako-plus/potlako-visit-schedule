800eb9f276cd5fa2e6642a9777f2cca533e7310c
tumiemosweu <imosweuk@gmail.com>
2020-09-10 22:17:09 +0200

    update visit schedule

--- potlako_visit_schedule/visit_schedules/crfs.py
+++ potlako_visit_schedule/visit_schedules/crfs.py
@@ -5,7 +5,7 @@ crf = {}
 potlako_crfs_prn = FormsCollection(
     Crf(show_order=1, model='potlako_subject.missedcall',
         required=False, additional=False),
-    Crf(show_order=2, model='potlako_subject.missedvisit',
+    Crf(show_order=2, model='potlako_subject.homevisit',
         required=False, additional=False),
     Crf(show_order=3, model='potlako_subject.sms',
         required=False, additional=False),
@@ -15,11 +15,10 @@ crfs_initial = FormsCollection(
     Crf(show_order=1, model='potlako_subject.patientcallinitial'),
     Crf(show_order=2, model='potlako_subject.symptomandcareseekingassessment'),
     Crf(show_order=3, model='potlako_subject.transport', required=False),
-    Crf(show_order=4, model='potlako_subject.homevisit', required=False),
-    Crf(show_order=5, model='potlako_subject.physicianreview', required=False),
-    Crf(show_order=6, model='potlako_subject.investigationsordered', required=False),
-    Crf(show_order=7, model='potlako_subject.investigationsresulted', required=False),
-    Crf(show_order=8, model='potlako_subject.medicaldiagnosis', required=False),
+    Crf(show_order=4, model='potlako_subject.physicianreview', required=False),
+    Crf(show_order=5, model='potlako_subject.investigationsordered', required=False),
+    Crf(show_order=6, model='potlako_subject.investigationsresulted', required=False),
+    Crf(show_order=7, model='potlako_subject.medicaldiagnosis', required=False),
     name='initial',
 )
 
@@ -27,17 +26,19 @@ crfs_unscheduled = FormsCollection(
     Crf(show_order=1, model='potlako_subject.patientcallfollowup',),
     Crf(show_order=2, model='potlako_subject.cancerdiagnosisandtreatmentassessment',),
     Crf(show_order=3, model='potlako_subject.transport', required=False),
-    Crf(show_order=4, model='potlako_subject.homevisit', required=False),
-    Crf(show_order=5, model='potlako_subject.physicianreview', required=False),
-    Crf(show_order=6, model='potlako_subject.investigationsordered', required=False),
-    Crf(show_order=7, model='potlako_subject.investigationsresulted', required=False),
-    Crf(show_order=8, model='potlako_subject.medicaldiagnosis', required=False),
+    Crf(show_order=4, model='potlako_subject.missedvisit', required=False),
+    Crf(show_order=5, model='potlako_subject.investigationsordered', required=False),
+    Crf(show_order=6, model='potlako_subject.investigationsresulted', required=False),
+    Crf(show_order=7, model='potlako_subject.medicaldiagnosis', required=False),
     name='post_unscheduled',
 )
 
 crfs_followup = FormsCollection(
     Crf(show_order=1, model='potlako_subject.patientcallfollowup'),
     Crf(show_order=2, model='potlako_subject.cancerdiagnosisandtreatmentassessment'),
+    Crf(show_order=3, model='potlako_subject.investigationsordered', required=False),
+    Crf(show_order=4, model='potlako_subject.investigationsresulted', required=False),
+    Crf(show_order=5, model='potlako_subject.missedvisit', required=False),
     name='followup',
 )
 
