5acf87bceac918d86278deb172cc54703f4b2b6e
tumiemosweu <imosweuk@gmail.com>
2020-09-11 10:21:00 +0200

    fix visit schedule crfs

--- potlako_visit_schedule/visit_schedules/crfs.py
+++ potlako_visit_schedule/visit_schedules/crfs.py
@@ -29,16 +29,16 @@ crfs_unscheduled = FormsCollection(
     Crf(show_order=4, model='potlako_subject.missedvisit', required=False),
     Crf(show_order=5, model='potlako_subject.investigationsordered', required=False),
     Crf(show_order=6, model='potlako_subject.investigationsresulted', required=False),
-    Crf(show_order=7, model='potlako_subject.medicaldiagnosis', required=False),
     name='post_unscheduled',
 )
 
 crfs_followup = FormsCollection(
     Crf(show_order=1, model='potlako_subject.patientcallfollowup'),
     Crf(show_order=2, model='potlako_subject.cancerdiagnosisandtreatmentassessment'),
-    Crf(show_order=3, model='potlako_subject.investigationsordered', required=False),
-    Crf(show_order=4, model='potlako_subject.investigationsresulted', required=False),
-    Crf(show_order=5, model='potlako_subject.missedvisit', required=False),
+    Crf(show_order=3, model='potlako_subject.transport', required=False),
+    Crf(show_order=4, model='potlako_subject.investigationsordered', required=False),
+    Crf(show_order=5, model='potlako_subject.investigationsresulted', required=False),
+    Crf(show_order=6, model='potlako_subject.missedvisit', required=False),
     name='followup',
 )
 
